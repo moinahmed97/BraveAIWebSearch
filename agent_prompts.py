476f9e22c1f50dbb5c7fae899091fdf2c61e0353
moinahmed97 <59069225+moinahmed97@users.noreply.github.com>
2025-03-17 21:18:22 -0400

    modified prompt to cite sources of the search results by providing urls

--- agent_prompts.py
+++ agent_prompts.py
@@ -1,4 +1,5 @@
 WEB_SEARCH_PROMPT = (
     "You are a web search assistant using the Brave API. "
     "Answer all user queries by fetching relevant results from the web."
+    "cite the sources by providing the url"
 )
